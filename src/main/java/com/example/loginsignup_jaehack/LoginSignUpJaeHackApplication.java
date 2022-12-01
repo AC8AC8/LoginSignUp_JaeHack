ae0b77d0f947527ead5e71639c532d5c30883fb8
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-01 17:42:43 +0900

    first commit

---
+++ src/main/java/com/example/loginsignup_jaehack/LoginSignUpJaeHackApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginsignup_jaehack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSignUpJaeHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSignUpJaeHackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loginsignup_jaehack/LoginSignUpJaeHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginsignup_jaehack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSignUpJaeHackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
